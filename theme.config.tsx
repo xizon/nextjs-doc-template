eb8e0d911c84ed095223cac42e2b7f4d511ba428
UIUX Lab <uiuxlab@gmail.com>
2024-08-03 00:41:29 +0800

    update theme.config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import Image from 'next/image';
-
+import { useRouter } from 'next/router';
 
 const config: DocsThemeConfig = {
     logo: <div className="app-brand"><span><Image width={0} height={0} sizes="100vw" style={{ width: '140px', height: 'auto' }} src="/assets/images/logo.png" alt={''} /></span><span style={{display: 'none'}}><Image width={0} height={0} sizes="100vw" style={{ width: '140px', height: 'auto' }} src="/assets/images/logo-dark.png" alt={''} /></span></div>,
@@ -18,7 +18,15 @@ const config: DocsThemeConfig = {
     },
     feedback: {
         useLink: () => 'https://github.com/xizon/nextjs-doc-template',
-    } 
+    },
+    useNextSeoProps() {
+        const { asPath } = useRouter()
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s – Custom Title'
+            }
+        }
+    },
 }
 
 
