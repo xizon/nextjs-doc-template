726279f4a155415c4fb3c42b2ba4d3860c493ab1
UIUX Lab <uiuxlab@gmail.com>
2024-08-12 11:15:41 +0800

    update theme.config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
     logo: <div className="app-brand"><span><Image width={0} height={0} sizes="100vw" style={{ width: '140px', height: 'auto' }} src="/assets/images/logo.png" alt={''} /></span><span style={{display: 'none'}}><Image width={0} height={0} sizes="100vw" style={{ width: '140px', height: 'auto' }} src="/assets/images/logo-dark.png" alt={''} /></span></div>,
     docsRepositoryBase: 'https://github.com/xizon/nextjs-doc-template',
     project: {
         link: 'https://github.com/xizon/nextjs-doc-template',
+    },
+    head: () => {
+        const { asPath, defaultLocale, locale } = useRouter()
+        const { frontMatter } = useConfig()
+        const url =
+            'https://xizon.github.io' +
+            (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+        return (
+            <>
+                <meta property="og:url" content={url} />
+                <meta property="og:title" content={frontMatter.title || 'Custom Title'} />
+                <meta
+                    property="og:description"
+                    content={frontMatter.description || 'Custom Title'}
+                />
+            </>
+        )
     },
     footer: {
         text: (
